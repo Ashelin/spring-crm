985946b070c4016e8f2f75e8f8f6dd16aecc336e
Danylo Shelest <daniel.shelest1@gmail.com>
2022-10-16 21:58:16 +0200

    Install, offline config

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -16,7 +16,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
-@PWA(name = "Flow CRM Tutorial", shortName = "Flow CRM Tutorial", offlineResources = {})
+@PWA(name = "Flow CRM",
+        shortName = "Flow CRM",
+        offlinePath = "offline.html",
+        offlineResources = {"icons/icon.png", "images/offline.webp"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
